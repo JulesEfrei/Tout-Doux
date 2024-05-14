53ac7c0ed145ed0af09f7282de7358ac81dcf6a3
JulesEfrei <jules.bruzeau@efrei.net>
2024-05-14 17:02:24 +0200

    initial commit

---
+++ src/main/java/com/efrei/toutdoux/ToutdouxApplication.java
@@ -0,0 +1,13 @@
+package com.efrei.toutdoux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToutdouxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToutdouxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/efrei/toutdoux/ToutdouxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efrei.toutdoux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToutdouxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
